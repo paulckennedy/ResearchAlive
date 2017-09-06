4c9bb4e2c853a9a78793deb2403f07042e943635
Paul Kennedy <paulcalvinkennedy@gmail.com>
2017-09-06 15:02:29 -0500

    Initial Commit

---
+++ app/components/note.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { 
+  AppRegistry,
+  StyleSheet,
+  Text, 
+  View,
+  TouchableOpacity 
+} from 'react-native';
+
+export default class Note extends React.Component {
+  render() {
+    return (
+      <View key={this.props.keyval} style={styles.note}>
+            <Text style={styles.noteText}>{this.props.val.date}</Text>
+            <Text style={styles.noteText}>{this.props.val.note}</Text>
+
+            <TouchableOpacity onpress={this.props.deletMethod} style={styles.noteDelete}>
+                <Text style={styles.noteDeletText}>Delete</Text>
+            </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  note: {
+    position: "relative",
+    padding: 20,
+    paddingRight: 100,
+    borderBottomWidth: 2,
+    borderBottomColor: "#ededed",
+  },
+  noteText: {
+      paddingLeft: 20,
+      borderLeftWidth: 10,
+      borderLeftColor: "#E91E63",
+  },
+  noteDelete: {
+      position: "absolute",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: '#298069',
+      padding: 10,
+      top: 10,
+      bottom: 10,
+      right: 10,
+  },
+  noteDeleteText: {
+      color: "white",
+  }
+});
